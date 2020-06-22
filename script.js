3bb24d47d6a7b644977564f6dcb7da0536059bb3
Nader Frigui <nadfri@gmail.com>
2020-06-22 12:16:56 +0200

    ajout du blur sur element actif si la page perd sa visibilité

--- script.js
+++ script.js
@@ -12,9 +12,7 @@ labelDuree.classList     .add("slide");
 spanPrixAttente.classList.add("slide");
 labelDistance.classList  .add("slide");
 spanBase.classList       .add("slide");
-//Mise à jour du prix de base
-spanBase.textContent = "+" + base + "€";
-
+spanBase.textContent = "+" + base + "€"; //Mise à jour du prix de base
 
 uberX.onclick = () => {
     //Mise à jour des prix
@@ -70,17 +68,31 @@ const liveUpdate = (input,span) =>{
     if(span == spanDuree && duree == 0.00) span.classList.remove("slide");
     else if(input.value == "")             span.classList.remove("slide");
     
-
     divRevenu.classList.add("scale15");
     setTimeout(()=>{divRevenu.classList.remove("scale15")},300);
 };
-//----------
+//----------Gestion des focus---------------------------
+let hidden, visibilityChange;
+if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
+  hidden = "hidden";
+  visibilityChange = "visibilitychange";
+} else if (typeof document.msHidden !== "undefined") {
+  hidden = "msHidden";
+  visibilityChange = "msvisibilitychange";
+} else if (typeof document.webkitHidden !== "undefined") {
+  hidden = "webkitHidden";
+  visibilityChange = "webkitvisibilitychange";
+}
+//Quand la page perd la visibilité, le focus d'un input actif est enlevé pour eviter son effacement
+document.addEventListener(visibilityChange, ()=>{document.activeElement.blur()}, false);
+
 hour.onfocus     = () => {hour.value     = ""; calcul();if(duree == 0.00) spanDuree.classList.remove("slide");} 
 minute.onfocus   = () => {minute.value   = ""; calcul();if(duree == 0.00) spanDuree.classList.remove("slide");} 
 seconde.onfocus  = () => {seconde.value  = ""; calcul();if(duree == 0.00) spanDuree.classList.remove("slide");} 
 distance.onfocus = () => {distance.value = ""; liveUpdate(distance,spanDistance);};
 attente.onfocus  = () => {attente.value  = ""; liveUpdate(attente,spanAttente);};
 
+//----------Gestion des inputs en temps réel--------------
 hour.oninput    = () => liveUpdate(hour,spanDuree);
 minute.oninput  = () => liveUpdate(minute,spanDuree);
 seconde.oninput = () => liveUpdate(seconde,spanDuree);
@@ -103,7 +115,8 @@ attente.oninput   = () => {
 
     liveUpdate(attente,spanAttente);
 };
- 
+
+//----------Gestion des calculs-------------- 
 const calcul   = () => {
     duree = ((Number(hour.value)*60 + Number(minute.value) + Number(seconde.value)/60)*prixDuree).toFixed(2);
     spanDuree.textContent = "+" + duree + "€";
@@ -155,7 +168,6 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 const metas = document.getElementsByTagName('meta');
 metas[1].content = 'width=device-width, height=' + window.innerHeight + ' initial-scale=1.0, maximum-scale=5.0,user-scalable=0';
-
 
 /*********************************** */
 };
