0c88ac56753ec8fe2edb8af0c1a63e95954cd169
Nader Frigui <nadfri@gmail.com>
2020-09-06 18:46:07 +0200

    Modification WorkBox

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 if (workbox) {
-  console.log(`WorkBox of uber-verif loaded🎉`);
-  workbox.routing.registerRoute(
-    new RegExp('/*'), //cached all files
-    new workbox.strategies.StaleWhileRevalidate()
-  );
-} else {console.log(`Boo! Workbox didn't load 😬`);}
+  console.log(`WorkBox2 of uber-verif loaded🎉`);
+  // workbox.routing.registerRoute(
+  //   new RegExp('/*'), //cached all files
+  //   new workbox.strategies.StaleWhileRevalidate()
+  // );
+  workbox.precaching.precacheAndRoute([]);
+
+} else {console.log(`Boo! Workbox2 didn't load 😬`);}
